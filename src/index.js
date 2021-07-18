99a85126299fc7b4335a202719fc94e74e870886
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-18 21:52:11 +0900

    削除機能を実装。バニラやったことないから案外詰まるねw

--- src/index.js
+++ src/index.js
@@ -10,8 +10,24 @@ const onClickAdd = () => {
   const div = document.createElement('div');
   div.innerHTML = inputText;
 
+  const completeButton = document.createElement('button');
+  completeButton.innerText = 'Fin';
+  completeButton.addEventListener('click', () => {
+    alert();
+  });
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'Delete';
+  deleteButton.addEventListener('click', () => {
+    //親のliを削除する
+    const deleteTarget = deleteButton.parentNode; //parentNodeで親要素を取得する
+    document.getElementById('incomplete_list').removeChild(deleteTarget);
+  });
+
   //liのなかにdivを定義
   li.appendChild(div);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById('incomplete_list').appendChild(li);
@@ -20,3 +36,12 @@ const onClickAdd = () => {
 document.getElementById('add-button').addEventListener('click', () => {
   onClickAdd();
 });
+
+const deleteButtonList = document.getElementsByClassName('delete_task');
+for (let i = 0; i < deleteButtonList.length; i++) {
+  const deleteButtonTag = deleteButtonList[i];
+  deleteButtonTag.addEventListener('click', () => {
+    const deleteTargetDef = deleteButtonTag.parentNode;
+    document.getElementById('incomplete_list').removeChild(deleteTargetDef);
+  });
+}
