0ebcfe6a4652d28cafff8a64edd148a678e96372
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-18 21:08:09 +0900

    タスクの追加機能を実装。基本の確認だけど忘れてるな〜。

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-import "./styles.css"
+const onClickAdd = () => {
+  //inputを取得する
+  const inputText = document.getElementById('add_text').value;
+  document.getElementById('add_text').value = '';
 
+  //create div
+  const li = document.createElement('li');
+  li.className = 'list-row';
+
+  const div = document.createElement('div');
+  div.innerHTML = inputText;
+
+  //liのなかにdivを定義
+  li.appendChild(div);
+
+  //未完了リストに追加
+  document.getElementById('incomplete_list').appendChild(li);
+};
+
+document.getElementById('add-button').addEventListener('click', () => {
+  onClickAdd();
+});
